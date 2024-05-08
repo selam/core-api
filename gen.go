19b820a1e648e90b3ca03329e91080dc380e40f9
timu eren <selamtux@gmail.com>
2024-05-08 15:22:33 +0300

    change package name

--- gen.go
+++ gen.go
@@ -1,4 +1,4 @@
-package main
+package gen
 
 //go:generate protoc -I. --go_out=.  --go_opt=paths=source_relative core_search.proto
 //go:generate protoc -I. --go-grpc_out=.  --go-grpc_opt=paths=source_relative  core_service.proto
